328e4ed0a03af94caad5de05c595ff2c8360d198
Quan Afoon <quanafoon@gmail.com>
2024-10-09 01:42:22 +0000

    all functionality working, still some bugs

--- App/views/student.py
+++ App/views/student.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 from.index import index_views
 
 from App.controllers import (
     create_student,
     get_student,
-    add_review,
     jwt_required
 )
 
@@ -46,48 +45,3 @@ def search_result():
         flash('Student was not found')
         return render_template('search.html')
 
-@student_views.route('/review', methods=['GET'])
-@jwt_required()
-def review_student():
-    return render_template('reviewForm.html')
-
-
-@student_views.route('/addReview', methods=['POST'])
-@jwt_required()
-def addReview():
-    userID = get_jwt_identity()
-    data = request.form
-    review = add_review(data['studentID'], userID, data['text'])
-    if review:
-        flash('Review added')
-    else:
-        flash('Review could not be added')
-    return render_template('reviewForm.html')
-
-
-@student_views.route('/viewReviews', methods=['GET'])
-@jwt_required()
-def view_reviews():
-    return render_template('reviews.html')
-
-@student_views.route('/students', methods=['POST'])
-def create_student_action():
-    data = request.form
-    flash(f"student {data['studentname']} created!")
-    create_student(data['studentname'], data['password'])
-    return redirect(url_for('student_views.get_student_page'))
-
-@student_views.route('/api/students', methods=['GET'])
-def get_students_action():
-    students = get_all_students_json()
-    return jsonify(students)
-
-@student_views.route('/api/students', methods=['POST'])
-def create_student_endpoint():
-    data = request.json
-    student = create_student(data['studentname'], data['password'])
-    return jsonify({'message': f"student {student.studentname} created with id {student.id}"})
-
-@student_views.route('/static/students', methods=['GET'])
-def static_student_page():
-  return send_from_directory('static', 'static-student.html')
